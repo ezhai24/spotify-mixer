2df11bb338907f72ec32e12d553a1ecd24f8d930
Emily Zhai <emily.zhai@convoy.com>
2020-05-09 19:44:04 -0700

    refactoring session functions into separate file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,56 +1,8 @@
-import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-
 admin.initializeApp();
 
-exports.createSession = functions.https.onCall(async (data) => {
-  const { displayName } = data;
-  const session = await admin.firestore().collection('sessions').add({
-    users: [displayName],
-  });
-  return {
-    sessionId: session.id,
-  };
-});
-
-exports.endSession = functions.https.onRequest(async (req, res) => {
-  const { method } = req;
-  if (method === 'POST') {
-    const { body } = req;
-    const { sessionId } = JSON.parse(body);
-    await admin.firestore().collection('sessions').doc(sessionId).delete();
-  }
-  res.end();
-});
-
-exports.joinSession = functions.https.onCall(async (data) => {
-  const { displayName, sessionId } = data;
-
-  const session = await admin.firestore().collection('sessions').doc(sessionId).get();
-  const sessionUsers = session.get('users');
-  if (sessionUsers.includes(displayName)) {
-    throw new functions.https.HttpsError(
-      'already-exists',
-      'A user with this name has already joined this session',
-    );
-  }
-
-  await admin.firestore().collection('sessions').doc(sessionId).update({
-    users: admin.firestore.FieldValue.arrayUnion(displayName),
-  });
-
-  return null;
-});
-
-exports.leaveSession = functions.https.onRequest(async (req, res) => {
-  const { method } = req;
-  if (method === 'POST') {
-    const { body } = req;
-    const { displayName, sessionId } = JSON.parse(body);
-    await admin.firestore().collection('sessions').doc(sessionId).update({
-      users: admin.firestore.FieldValue.arrayRemove(displayName),
-    });
-  }
-  res.end();
-});
-
+const sessions = require('./sessions');
+exports.createSession = sessions.createSession;
+exports.joinSession = sessions.joinSession;
+exports.leaveSession = sessions.leaveSession;
+exports.endSession = sessions.endSession;
---
+++ functions/src/sessions.ts
@@ -0,0 +1,54 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+exports.createSession = functions.https.onCall(async (data) => {
+  const { displayName } = data;
+  const session = await admin.firestore().collection('sessions').add({
+    users: [displayName],
+  });
+  return {
+    sessionId: session.id,
+  };
+});
+
+exports.endSession = functions.https.onRequest(async (req, res) => {
+  const { method } = req;
+  if (method === 'POST') {
+    const { body } = req;
+    const { sessionId } = JSON.parse(body);
+    await admin.firestore().collection('sessions').doc(sessionId).delete();
+  }
+  res.end();
+});
+
+exports.joinSession = functions.https.onCall(async (data) => {
+  const { displayName, sessionId } = data;
+
+  const session = await admin.firestore().collection('sessions').doc(sessionId).get();
+  const sessionUsers = session.get('users');
+  if (sessionUsers.includes(displayName)) {
+    throw new functions.https.HttpsError(
+      'already-exists',
+      'A user with this name has already joined this session',
+    );
+  }
+
+  await admin.firestore().collection('sessions').doc(sessionId).update({
+    users: admin.firestore.FieldValue.arrayUnion(displayName),
+  });
+
+  return null;
+});
+
+exports.leaveSession = functions.https.onRequest(async (req, res) => {
+  const { method } = req;
+  if (method === 'POST') {
+    const { body } = req;
+    const { displayName, sessionId } = JSON.parse(body);
+    await admin.firestore().collection('sessions').doc(sessionId).update({
+      users: admin.firestore.FieldValue.arrayRemove(displayName),
+    });
+  }
+  res.end();
+});
+
