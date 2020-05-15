fcdbb6edf4d25a5f66e88d701b47a998e581bcae
Emily Zhai <emily.zhai@convoy.com>
2020-05-14 21:14:59 -0700

    adding general styles

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { Global } from '@emotion/core';
+import colors from 'shared/colors';
+
+class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+          <Global
+            styles={{
+              'html, body': {
+                fontFamily: 'IBM Plex Sans',
+                backgroundColor: colors.backgroundLight,
+                color: colors.primaryText,
+              },
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document;
---
+++ shared/colors.ts
@@ -0,0 +1,8 @@
+export default {
+  primary: '#1FB954',
+  background: '#121212',
+  backgroundLight: '#282828',
+  backgroundDark: '#040404',
+  primaryText: 'white',
+  secondaryText: '#B3B3B3',
+};
