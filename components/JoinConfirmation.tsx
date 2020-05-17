93d563855920507e4836d8e94d5d0d2ca39f5d94
Emily Zhai <emily.zhai@convoy.com>
2020-05-16 23:16:06 -0700

    styling join confirmation page

--- components/JoinConfirmation.tsx
+++ components/JoinConfirmation.tsx
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { SessionUser } from '~/shared/types';
+import styled from '@emotion/styled';
 
+import { PrimaryButton } from '~/components/Form';
 import { firestore } from '~/services/firebase';
+import { SessionUser } from '~/shared/types';
 
 interface Props {
   currentUser: SessionUser;
 }
+
+const PageContainer = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  margin: '150px 20px',
+  textAlign: 'center',
+});
+
+const Message = styled.div({
+  margin: 10,
+  p: {
+    margin: 0,
+  },
+});
 
 const JoinConfirmation = (props: Props) => {
   const { currentUser } = props;
@@ -20,15 +37,31 @@ const JoinConfirmation = (props: Props) => {
       }
     });
   });
-
+  
   return (
-    <>
-      { sessionIsRunning ?
-        <p>You've joined a session!</p>
-        :
-        <p>Session has been ended by creator :(</p>
+    <PageContainer>
+      <h1 style={{ margin: 0 }}>
+        { sessionIsRunning ? 'Thanks for joining!' : 'Session ended' }
+      </h1>
+      <Message>
+        { sessionIsRunning ?
+          <>
+            <p>You are now in the session.</p>
+            <p>Please leave this window open to remain.</p>
+          </>
+          :
+          <>
+            <p>The original creator of this session has ended it.</p>
+            <p>Feel free to close the browser window.</p>
+          </>
+        }
+      </Message>
+      { sessionIsRunning &&
+        <PrimaryButton style={{ width: 200, fontWeight: 'normal' }}>
+          Leave
+        </PrimaryButton>
       }
-    </>
+    </PageContainer>
   );
 };
 
